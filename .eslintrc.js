5c780fb6e608e738fbdf847f0a94286de4c3dffa
jarrodwatts <35651410+jarrodwatts@users.noreply.github.com>
2021-05-01 16:19:37 +1000

    Add Material UI and mimick Trello colours

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,9 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  rules: {
+    'require-jsdoc': 0,
+  },
   settings: {
     react: {
       version: 'latest',
